fd567b8419cad3fca4aa27730982b06b81d98d4a
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-05-24 15:26:37 +0530

    use Router and context

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { QuizProvider } from "./context/quiz-context";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <QuizProvider>
+      <Router>
+        <App />
+      </Router>
+    </QuizProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
