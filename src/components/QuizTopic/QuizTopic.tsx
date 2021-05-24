fc8f8672e4170030e7bb6672b519f50b3223e792
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-05-24 15:24:06 +0530

    refactor : using props to display quiz data

--- src/components/QuizTopic/QuizTopic.tsx
+++ src/components/QuizTopic/QuizTopic.tsx
@@ -1,21 +1,31 @@
-export default function QuizTopic() {
+import { Link } from "react-router-dom";
+import { Quiz } from "../../data/quiz.type";
+
+type QuizTopicProp = {
+  quiz: Quiz;
+};
+
+export default function QuizTopic({ quiz }: QuizTopicProp) {
+  const { id, name, imageURL } = quiz;
   return (
-    <div className="rounded overflow-hidden ">
-      <div className="relative h-5/6">
-        <img
-          className="w-full h-auto"
-          src="https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
-          alt="card-bg"
-        />
-        <div className="absolute top-0 left-0 h-full w-full bg-gray-800 bg-opacity-60 flex items-center justify-center">
-          <p className="text-lg font-semibold text-white">UX Fundamentals</p>
+    <Link to={`/quiz/${id}`}>
+      <div className="rounded overflow-hidden h-44">
+        <div className="relative h-4/5">
+          <img
+            className="w-full h-full object-cover"
+            src={imageURL}
+            alt="card-bg"
+          />
+          <div className="absolute top-0 left-0 h-full w-full bg-gray-800 bg-opacity-60 flex items-center justify-center">
+            <p className="text-lg font-semibold text-white">{name}</p>
+          </div>
         </div>
+        <button className="text-lg italic font-bold text-center text-white bg-purple-500 w-full h-1/5">
+          PLAY QUIZ {">"}
+          {">"}
+          {">"}
+        </button>
       </div>
-      <button className="text-lg italic font-bold text-center text-white bg-purple-500 w-full p-2">
-        PLAY QUIZ {">"}
-        {">"}
-        {">"}
-      </button>
-    </div>
+    </Link>
   );
 }
