9c80dd0d21c875ac396e71c86eacb30cc4d70af7
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-05-24 15:23:05 +0530

    feat : display all quizzes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,21 @@
+import { useEffect } from "react";
 import { Navbar, QuizTopic } from "../../components";
+import { useQuiz } from "../../context/quiz-context";
+import { quizzes } from "../../data/database";
 
 export default function Home() {
+  const { dispatch } = useQuiz();
+
+  useEffect(() => dispatch({ type: "RESET_QUIZ" }), [dispatch]);
   return (
     <>
       <Navbar />
       <main className="p-4">
-        <QuizTopic />
+        <div className="grid xl:grid-cols-3 lg:grid-cols-2  grid-cols-1 gap-4">
+          {quizzes.map((quiz) => (
+            <QuizTopic key={quiz.id} quiz={quiz} />
+          ))}
+        </div>
       </main>
     </>
   );
