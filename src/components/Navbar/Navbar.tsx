3d5a737490e54b84933ff2da2331ad54e06e08ae
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-05-24 15:47:26 +0530

    update quiz logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <nav className="max-w-full flex justify-center p-4 mb-6 bg-gray-800 sticky top-0 left-0">
       <span className="text-base font-extrabold px-2 py-1 text-white border-2 border-white">
-        QUIZ•UP
+        JARVIS•QUIZ
       </span>
     </nav>
   );
