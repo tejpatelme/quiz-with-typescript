a74ac62e1fbfc1b9834d32b0b1a74af0de95d938
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-07-13 17:56:21 +0530

    re-exports components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,7 @@
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as QuizTopic } from "./QuizTopic/QuizTopic";
+export { default as PrivateRoute } from "./PrivateRoute/PrivateRoute";
+export { default as Icon } from "./Icon/Icon";
+export { default as SidebarNavlink } from "./SidebarNavlink/SidebarNavlink";
+export { default as Sidebar } from "./Sidebar/Sidebar";
+export { default as ResultCard } from "./ResultCard/ResultCard";
