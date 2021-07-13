02a9ab235317089106c97c37de0fae098f347810
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-07-13 17:56:00 +0530

    style : add mobile responsiveness

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,19 @@
-export default function Navbar() {
+import React from "react";
+
+type NavbarProp = {
+  setShowSidebar: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function Navbar({ setShowSidebar }: NavbarProp) {
+  const onHamburgerClick = () => setShowSidebar((showSidebar) => !showSidebar);
   return (
-    <nav className="max-w-full flex justify-center p-4 mb-6 bg-gray-800 sticky top-0 left-0">
+    <nav className="sm:hidden max-w-full p-4 sm:mb-4 flex justify-center bg-gray-800 sticky z-10 top-0 left-0">
+      <button onClick={onHamburgerClick}>
+        <span className="material-icons-round absolute top-[1rem] left-[1rem] text-white text-3xl sm:hidden">
+          menu
+        </span>
+      </button>
+
       <span className="text-base font-extrabold px-2 py-1 text-white border-2 border-white">
         JARVIS•QUIZ
       </span>
